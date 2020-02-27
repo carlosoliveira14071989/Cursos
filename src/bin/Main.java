0a6280f754dfc96678b4f24f7415b7fd5d580e43
unknown <carlosoliveira@hotmail.com.br>
2020-02-27 15:10:08 -0300

    atualizando

---
+++ src/bin/Main.java
@@ -0,0 +1,14 @@
+package bin;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		double x = 1.28222;
+		System.out.println("Ola mundo");
+		System.out.println("bom dia");
+		System.out.printf("%.2f%n",x);
+
+	}
+
+}
